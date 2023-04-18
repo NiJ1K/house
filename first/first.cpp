3efb0acfe2b7a1eeb21345423f7e7cbdaa61be89
Maria <Maria@MSI>
2023-04-18 19:42:44 +0300

    Добавьте файлы проекта.

---
+++ first/first.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+    std::string house = "         *     *\n";
+    house += "       *   *  **\n";
+    house += "     *       * *  \n";
+    house += "   *           *   \n";
+    house += " *               *  \n";
+    house += "*********************\n";
+    house += "                     *\n";
+    house += "              * *    *\n";
+    house += "      ******* * *    *   *   *   *\n";
+    house += "      *  *  *        *  * * * * * *\n";
+    house += "      *     *        *  * * * * * *\n";
+    house += "***********************************\n";
+    std::cout << house << std::endl;
+    return 0;
+}
